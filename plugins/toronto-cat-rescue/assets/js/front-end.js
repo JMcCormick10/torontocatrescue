079a0935dd4c984c8a8c6f8a465343e8799d8537
Josh McCormick <jmccormick1024@gmail.com>
2018-11-17 16:48:45 -0500

    fixed modal scoll

--- plugins/toronto-cat-rescue/assets/js/front-end.js
+++ plugins/toronto-cat-rescue/assets/js/front-end.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($){
     $('.tcr-apply-to-adopt').click(function(){
         $('.tcr-cat-modal').addClass('tcr-cat-modal-show');
+
     });
     $('.tcr-exit-modal').click(function(){
         $('.tcr-cat-modal').removeClass('tcr-cat-modal-show');
